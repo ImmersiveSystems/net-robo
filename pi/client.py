3b6a19b9cd62a1a22df4000f6579e5ced6fbee17
George Xu <georgemxu@gmail.com>
2014-01-14 14:39:19 -0800

    Threading

--- pi/client.py
+++ pi/client.py
@@ -1,8 +1,9 @@
 from socketIO_client import SocketIO
 
 import serial
+import threading
 
-ser = serial.Serial("/dev/ttyUSB0", 115200)
+#ser = serial.Serial("/dev/ttyUSB0", 115200)
 print 'opened serial'
 
 speed1 = 40
@@ -12,50 +13,56 @@ speed3 = 100
 speed = speed1
 
 def listener(*args):
-        global speed
-        print 'recieved a command'
-        if args[0] == 'forward':
-                print 'w'
-                ser.write('HB' + chr(2) + chr(speed) + chr(2) + chr(speed))
+    global speed
+    print 'recieved a command'
+    if args[0] == 'forward':
+        print 'w'
+        #ser.write('HB' + chr(2) + chr(speed) + chr(2) + chr(speed))
 
-        elif args[0] == '-forward' or args[0] == '-right' or args[0] == '-left'  o$
-                print 'stop'
-                ser.write('HB' + chr(2) + chr(0) + chr(2) + chr(0))
+    elif (args[0] == '-forward') or (args[0] == '-right') or (args[0] == '-left') or (args[0] == '-backward'):
+        print 'stop'
+        #ser.write('HB' + chr(2) + chr(0) + chr(2) + chr(0))
 
-        elif args[0] == 'backward':
-                print 's'
-                ser.write('HB' + chr(0) + chr(speed) + chr(0) + chr(speed))
+    elif args[0] == 'backward':
+        print 's'
+        #ser.write('HB' + chr(0) + chr(speed) + chr(0) + chr(speed))
 
-        elif args[0] == 'right':
-                print 'r'
-                ser.write('HB' + chr(2) + chr(speed) + chr(0) + chr(speed))
+    elif args[0] == 'right':
+        print 'r'
+        #ser.write('HB' + chr(2) + chr(speed) + chr(0) + chr(speed))
 
-        elif args[0] == 'left':
-                print 'l'
-                ser.write('HB' + chr(0) + chr(speed) + chr(2) + chr(speed))       $
+    elif args[0] == 'left':
+        print 'l'
+         #ser.write('HB' + chr(0) + chr(speed) + chr(2) + chr(speed))
 
-        elif args[0] == 'stop':
-                print 'x'
-                ser.write('x')
-
-
-        elif args[0] == 'speed1':
-                speed = speed1
-        elif args[0] == 'speed2':
-                speed = speed2
-        elif args[0] == 'speed3':
-                speed = speed3
-
-        elif args[0] == 'shiftr':
-                speed = speed1
-
-        elif args[0] == 'shiftl':
-                speed = speed3
+    elif args[0] == 'stop':
+        print 'x'
+        #ser.write('x')
 
 
+    elif args[0] == 'speed1':
+        speed = speed1
+    elif args[0] == 'speed2':
+        speed = speed2
+    elif args[0] == 'speed3':
+        speed = speed3
 
-socketIO = SocketIO('192.168.1.13', 3000)
+    elif args[0] == 'shiftr':
+        speed = speed1
+
+    elif args[0] == 'shiftl':
+        speed = speed3
+
+def hello():
+    print 'HELLO!'
+    threading.Timer(1, hello).start()
+
+hello()
+
+socketIO = SocketIO('localhost', 3000)
 socketIO.on('serverToPython', listener)
 socketIO.emit('clientType', 'Python')
 socketIO.wait(seconds=6000)
+
+
 
